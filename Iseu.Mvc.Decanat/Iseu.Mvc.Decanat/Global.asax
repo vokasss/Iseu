40a0252f1f02d4b0d6a811022b25abae7ffd11b3
vokasss <krutalevich02@rambler.ru>
2013-04-09 10:36:53 +0400

    Commit #2 VS 2012

--- Iseu.Mvc.Decanat/Iseu.Mvc.Decanat/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Iseu.Mvc.Decanat.MvcApplication" Language="C#" %>
---
+++ Iseu.Mvc.Decanat/Iseu.UI/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Iseu.UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View("~/views/home/index.cshtml");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View("~/views/home/about.cshtml");
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View("~/views/home/contact.cshtml");
+        }
+    }
+}
